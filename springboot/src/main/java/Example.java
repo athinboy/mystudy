2efdb9ceed87df0421d0c7507c4c3b1e78ec7c21
athinboy <fgq841103@163.com>
2018-11-16 14:12:30 +0800

    dubbo client

---
+++ springboot/src/main/java/Example.java
@@ -0,0 +1,23 @@
+/**
+ * @author fenggqc
+ * @create 2018-11-02 16:48
+ **/
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}

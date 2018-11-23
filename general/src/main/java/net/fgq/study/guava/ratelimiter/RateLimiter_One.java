11c6ae23db9dbd849b0ef3eb714bd9c37dd013fd
athinboy <fgq841103@163.com>
2018-11-23 11:28:28 +0800

    rate limiter

--- general/src/main/java/net/fgq/study/guava/ratelimiter/RateLimiter_One.java
+++ general/src/main/java/net/fgq/study/guava/ratelimiter/RateLimiter_One.java
@@ -2,12 +2,17 @@ package net.fgq.study.guava.ratelimiter;
 
 import com.google.common.util.concurrent.RateLimiter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author fenggqc
  * @create 2018-11-21 17:55
  **/
-
-
 
 
 public class RateLimiter_One {
@@ -17,4 +22,34 @@ public class RateLimiter_One {
 
     //region Getter And Setter
     // endregion
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        rateLimiter = RateLimiter.create(2D, 1000L, TimeUnit.MILLISECONDS);
+
+
+        List<Runnable> runnableList = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            runnableList.add(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(finalI);
+                }
+            });
+        }
+
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (Runnable task : runnableList) {
+            rateLimiter.acquire(); // may wait
+            executorService.submit(task);
+
+        }
+
+    }
 }

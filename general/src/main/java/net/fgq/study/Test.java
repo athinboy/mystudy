53fe0ef9b19ac0741095f9c916cd31074422eb45
athinboy <fgq841103@163.com>
2018-12-25 15:49:29 +0800

    join

--- general/src/main/java/net/fgq/study/Test.java
+++ general/src/main/java/net/fgq/study/Test.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.builder.CompareToBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author fenggqc
@@ -19,6 +20,7 @@ public class Test {
 
     public static void main(String[] args) {
 
+        final Car car = Car.create( Car::new );
         System.out.println(a());
     }
 
@@ -31,6 +33,27 @@ public class Test {
             return false;
         }
 
+    }
+
+
+
+
+    public static class Car {
+        public static Car create( final Supplier< Car > supplier ) {
+            return supplier.get();
+        }
+
+        public static void collide( final Car car ) {
+            System.out.println( "Collided " + car.toString() );
+        }
+
+        public void follow( final Car another ) {
+            System.out.println( "Following the " + another.toString() );
+        }
+
+        public void repair() {
+            System.out.println( "Repaired " + this.toString() );
+        }
     }
 
 

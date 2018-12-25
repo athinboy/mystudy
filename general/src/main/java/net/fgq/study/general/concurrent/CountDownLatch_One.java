bfea8d14edd7d22d3fa75e3b017c75a084e6667b
athinboy <fgq841103@163.com>
2018-12-25 16:35:30 +0800

    CountDownLatch

---
+++ general/src/main/java/net/fgq/study/general/concurrent/CountDownLatch_One.java
@@ -0,0 +1,85 @@
+package net.fgq.study.general.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author fenggqc
+ * @create 2018-12-25 15:51
+ **/
+
+
+public class CountDownLatch_One extends Thread {
+
+
+    @Override
+    public void run() {
+
+
+        try {
+            System.out.println(String.format("      %s等待开始", String.valueOf(this.index)));
+            batchstart_CountDownLatch.await();
+            Thread.sleep(300);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(String.format("      %s完成计算", String.valueOf(this.index)));
+        batchend_CountDownLatch.countDown();
+
+
+    }
+
+
+    private Integer index;
+    private static CountDownLatch batchend_CountDownLatch;
+
+    private static CountDownLatch batchstart_CountDownLatch;
+
+    private static boolean running = true;
+
+
+    public CountDownLatch_One(Integer i) {
+        this.index = i;
+    }
+
+
+    //等待多个任务完成
+
+
+    public static void main(String[] args) {
+        CountDownLatch_One main = new CountDownLatch_One(0);
+
+
+        int batchsize = 10;
+
+        batchend_CountDownLatch = new CountDownLatch(batchsize);
+        batchstart_CountDownLatch = new CountDownLatch(1);
+
+        List<CountDownLatch_One> countDownLatchOneList = new ArrayList<>();
+        for (int i = 0; i < batchsize; i++) {
+            countDownLatchOneList.add(new CountDownLatch_One(i));
+        }
+        for (int i = 0; i < batchsize; i++) {
+            countDownLatchOneList.get(i).start();
+        }
+        try {
+
+            Thread.sleep(2000);
+
+            System.out.println("开始");
+            batchstart_CountDownLatch.countDown();
+            batchend_CountDownLatch.await();
+            System.out.println("完成");
+
+            batchstart_CountDownLatch.countDown();
+            running = false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ general/src/main/java/net/fgq/study/general/concurrent/CyclicBarrier_One.java
@@ -0,0 +1,18 @@
+package net.fgq.study.general.concurrent;
+
+/**
+ * @author fenggqc
+ * @create 2018-12-25 16:13
+ **/
+
+
+public class CyclicBarrier_One extends Thread {
+
+    //region Getter And Setter
+    // endregion
+
+
+
+
+
+}

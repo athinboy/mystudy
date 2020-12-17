658aa0cc277d5651b355027b264299d5f50a774f
fengguoqiang <fgq123!@#>
2020-12-17 17:29:16 +0800

    修改

---
+++ dotnet/src/rabbitmq/Core/Config.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+using System.IO;
+
+namespace Core
+{
+    public class Config
+    {
+
+
+
+        private static Config instance = null;
+
+        public static Config GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (typeof(Config))
+                {
+                    if (instance == null)
+                    {
+                        instance = new Config();
+                        var configurationBuilder = new ConfigurationBuilder()
+                                             .SetBasePath(Directory.GetCurrentDirectory())
+                                                                  .AddJsonFile("config.json");
+                        configurationBuilder.Build().Bind(instance);
+
+                    }
+
+
+
+                }
+            }
+            return instance;
+        }
+
+    }
+}

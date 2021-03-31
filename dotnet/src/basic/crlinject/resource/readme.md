0bb73ab5abc3c120d513e4d3c40bb1c10aff0a2d
fengguoqiang <fgq123!@#>
2021-03-31 18:15:06 +0800

    git file list

---
+++ dotnet/src/basic/crlinject/resource/readme.md
@@ -0,0 +1,4 @@
+﻿### 资料
+```
+http://www.java2s.com/Open-Source/CSharp_Free_Code/DotNet_Library/Download_NETAsm_a_JIT_Native_Code_Injection_Library.htm
+```
---
+++ dotnet/src/basic/gitfilelist/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace gitfilelist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine(System.Environment.CurrentDirectory);
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = " /c git ";
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;             
+            Process process = System.Diagnostics.Process.Start(processStartInfo);
+            StreamReader stream = process.StandardOutput;
+            string str= stream.ReadToEnd();
+            process.WaitForExit();
+           
+            Console.WriteLine(str);
+            Console.ReadLine();
+
+            //git log -1 --name-only --author fengguoqiang --pretty="%s"  -5
+
+
+
+        }
+    }
+}

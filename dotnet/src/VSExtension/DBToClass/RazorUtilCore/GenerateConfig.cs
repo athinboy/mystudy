ef281de22eedd242e5a13d02d8557becb08a5c2e
fengguoqiang <athinboy@users.noreply.github.com>
2021-07-26 17:16:45 +0800

    init

---
+++ dotnet/src/VSExtension/DBToClass/RazorUtilCore/GenerateConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateUtilCore
+{
+    public class GenerateConfig
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NamespaceName { get; set; } = "";
+
+
+
+
+         
+    }
+}
---
+++ dotnet/src/VSExtension/DBToClass/RazorUtilCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using GenerateUtilCore;
+using RazorEngineCore;
+using System;
+using System.IO;
+
+namespace RazorUtilCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRazorEngine razorEngine = new RazorEngine();
+            IRazorEngineCompiledTemplate template = razorEngine.Compile("Hello @Model.Name");
+
+            string result = template.Run(new
+            {
+                Name = "Alexander"
+            });
+
+            Console.WriteLine(result);
+
+            string curDir = System.Environment.CurrentDirectory;
+            string path = string.Format("{0}{1}{2}{1}{3}"
+                , curDir
+                , System.IO.Path.DirectorySeparatorChar
+                , "template"
+                , "SqlSugarEntity.txt");
+            string content = File.ReadAllText(path);
+            template = razorEngine.Compile(content);
+            Console.WriteLine(template.Run(new GenerateConfig()));
+       
+
+
+
+        }
+
+
+    }
+}

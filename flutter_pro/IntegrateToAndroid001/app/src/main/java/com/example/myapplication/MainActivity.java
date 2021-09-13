a5cea1358c1df650ba7ce7de4385318259a34794
fengguoqiang <athinboy@users.noreply.github.com>
2021-09-13 15:17:10 +0800

    Update MainActivity.java

--- flutter_pro/IntegrateToAndroid001/app/src/main/java/com/example/myapplication/MainActivity.java
+++ flutter_pro/IntegrateToAndroid001/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,32 @@
-package com.example.myapplication;
+package com.example.myapplication;//package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public FlutterEngine flutterEngine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Instantiate a FlutterEngine.
+        flutterEngine = new FlutterEngine(this);
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault());
+        FlutterEngineCache
+                .getInstance()
+                .put("my_engine_id", flutterEngine);
 
     }
 
@@ -20,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(
                 FlutterActivity
-                        .withNewEngine()
-                        .initialRoute("/my_route")
+                        .withCachedEngine("my_engine_id")
                         .build(view.getContext())
+                        //.withNewEngine()
+//                        .initialRoute("/my_route")
+//                        .build(view.getContext())
         );
     }
 }

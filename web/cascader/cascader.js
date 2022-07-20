8d8299597209c5021daad08d6a6de4b4a142527e
fengguoqiang <athinboy@users.noreply.github.com>
2022-07-20 16:48:01 +0800

    just_commit

--- web/cascader/cascader.js
+++ web/cascader/cascader.js
@@ -1,11 +1,40 @@
 'use strict';
+
+
+
+
 window.cascader = {
 
-
-    Cascader: function (option) {
+    //默认配置
+    defaultOption: {
+        placeHold: '选吧，你倒是选呀！',
+        multiSelect: false
+    },
+    build: function (domId, option) {
+        if (!domId) {
+            this.showError('dom id 为空！');
+            return;
+        }
         option = option || {};
+        let targeDom = $(domId);
+        if (targeDom.length == 0) {
+            this.showError(domId + '不存在！');
+            return;
+        }
+        targeDom.hide();
 
-    }
+        targeDom.append();
+
+
+
+
+    },
+    showError(msg) {
+        if (console && console.error) {
+            console.error(msg);
+        }
+    },
+    
 
 
 

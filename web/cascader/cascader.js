8b1f69546ffc0da1d56de7e01fe3be9a9c9815de
fengguoqiang <athinboy@users.noreply.github.com>
2022-09-15 17:34:42 +0800

    justcommit

--- web/cascader/cascader.js
+++ web/cascader/cascader.js
@@ -8,11 +8,12 @@
         window.cascader = {
             //默认配置
             defaultOption: {
-                debugging: false,
-                placeHold: '选吧，你倒是选呀！',
-                multiSelect: false,
-                targeDom: null,
-                cssclassprefix: 'jquery_',
+                debugging: false,//调试模式
+                placeHold: '选吧，你倒是选呀！',//输入框占位符
+                multiSelect: false,//多选
+                targeDom: null,//替代的目标html dom
+                cssclassprefix: 'jquery_',//css类前缀
+                hoverExpand: false,//鼠标hover时展开子菜单
                 data: []
             },
             htmlTemplate: {
@@ -91,46 +92,91 @@
                 };
 
                 //选项项目
-                function candidateItemMenuItem(_containerdom, _data, _option) {
+                function candidateItemMenuItem(_menu, _data, _option) {
+                    //debugger;
+                    console.assert(_menu != null, '_menu ==null');
+                    console.assert(_data != null, '_data ==null');
+                    console.assert(_option != null, '_option ==null');
                     this.data = _data;
                     this.option = _option;
-                    this.containerDom = _containerdom;
-                    this.dom = null;
-                    this.subMenu = [];
+                    this.itemMenu = _menu;
+                    this.itemDom = null;
+                    this.subMenu = null;
+                    this.itemIndex = 1;
                 }
                 candidateItemMenuItem.prototype = {
                     constructor: this.candidateItemMenuItem,
-                    render: function () {
+                    //显示子菜单
+                    showChildMenu: function () {
                         debugger;
+                        let _itemData = this.data;
+                        if (this.subMenu == null && _itemData.subData != null && _itemData.subData.length > 0) {
+
+                            let newMenu = new candidateItemMenu(this.itemMenu.menuPanelDom, _itemData.subData, this.option, this);
+                            this.subMenu = newMenu;
+
+                        }
+                        if (this.subMenu != null) {
+                            this.subMenu.render();
+                        }
+
+
+                    },
+                    render: function () {
+
                         let _dom = window.cascader.createDOM('menuItem', { "data": this.data, "option": this.option });
-                        this.dom = _dom;
-                        this.containerDom.append(_dom);
+                        this.itemDom = _dom;
+                        this.itemDom[0].cascaderMenuItem = this;
+                        _dom.mouseleave(function () {
+                            window.console.debug('mouse leave');
+                        });
+                        _dom.mouseenter(function () {
+                            window.console.debug('mouse enter');
+                            let _cascaderMenuItem = this.cascaderMenuItem;
+                            if (_cascaderMenuItem.option.hoverExpand) {
+                                _cascaderMenuItem.showChildMenu();
+                            }
+
+
+                        });
+                        _dom.click(function () {
+                            let _cascaderMenuItem = this.cascaderMenuItem;
+                            _cascaderMenuItem.showChildMenu();
+
+                        });
+                        //debugger;
+                        this.itemMenu.menuDom.append(_dom);
                     }
 
                 };
 
                 //选项菜单列表
-                function candidateItemMenu(_menuPanelDom,_data, _option) {
+                function candidateItemMenu(_menuPanelDom, _data, _option, _parentMenu) {
+                    console.assert(_menuPanelDom != null, '_menuPanelDom ==null');
+                    console.assert(_data != null, '_data ==null');
+                    console.assert(_option != null, '_option ==null');
                     this.data = _data;
                     this.option = _option;
                     this.muneItems = [];
                     this.menuDom = null;
-                    this.menuPanelDom=_menuPanelDom;
+                    this.menuPanelDom = _menuPanelDom;
+                    this.itemIndex = 1;
+                    this.parentMenu = _parentMenu;
                 }
-
                 candidateItemMenu.prototype = {
                     constructor: this.candidateItemMenu,
                     render: function () {
-
-                        debugger;
                         let _dom = window.cascader.createDOM('menu', { "option": this.option });
                         this.menuDom = _dom;
+                        this.menuDom.cascaderMenu = this;
                         this.menuPanelDom.append(_dom);
                         let _muneItems = this.muneItems;
                         let _menuDom = this.menuDom;
+                        let _option = this.option;
+                        let _itemMenu = this;
                         $.each(this.data, function (_index, _value) {
-                            debugger;
-                            let _newitem = new candidateItemMenuItem(_menuDom, _value);
+
+                            let _newitem = new candidateItemMenuItem(_itemMenu, _value, _option);
                             _muneItems.push(_newitem);
                             _newitem.render();
                         });
@@ -154,21 +200,21 @@
                         this.containerDom = _dom;
                         _dom = window.cascader.createDOM('cascaderPanel', { "option": this.option });
                         this.panelDom = _dom;
-                        debugger;
+                        this.panelDom[0].cascaderPanel = this;
                         console.info(this.containerDom);
                         this.containerDom.append(this.panelDom);
                         $('body').append(this.containerDom);
                         if (window.console && window.console.info) {
                             window.console.info(_inputContainer.dom.offset());
                         }
+
                         this.containerDom.css("left", _inputContainer.dom.offset().left + 'px');
                         this.containerDom.css("top", (_inputContainer.dom.offset().top + _inputContainer.dom.height) + 'px');
 
                     },
                     addMenu: function (_data, _option) {
-                        debugger;
-
-                        let newMenu = new candidateItemMenu(this.panelDom,_data, _option);
+                        //debugger;
+                        let newMenu = new candidateItemMenu(this.panelDom, _data, _option);
                         newMenu.render();
                         this.itemMenus.push(newMenu);
 
@@ -196,20 +242,22 @@
 
                         let _dom = window.cascader.createDOM('cascaderInstance', { "option": this.option });
                         _dom.on('click', function () {
+
                             let instance = this.cascader;
                             instance.showCandidate();
-                            debugger;
+
                         });
+
                         this.dom = _dom;
                         this.dom[0].cascader = this;
-                        debugger;
+
                         this.option.targeDom.hide();
                         this.option.targeDom.after(_dom);
                         this.inputContainer.render(this.dom);
                         this.candidatePanel.render(this.inputContainer);
-                        debugger;
+
                         if (this.option.data && this.option.data.length > 0) {
-                            this.candidatePanel.addMenu(this.option.data);
+                            this.candidatePanel.addMenu(this.option.data, this.option);
                         }
 
                     },
